89389651a605e607e08bd628882690c21695306e
txyuqiwang <txyuqiwang@tencent.com>
2022-12-03 18:53:10 +0800

    feat : study 流程控制 continue goto

---
+++ processControl/0408-goto.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eric on 2022/12/3.
+//
+#include <iostream>
+using  namespace std ;
+int  main0408(){
+    cout << "1、xxxxxxxx" << endl ;
+    goto FLAG ;
+    cout << "2、xxxxxxxx" << endl ;
+    cout << "3、xxxxxxxx" << endl ;
+    cout << "4、xxxxxxxx" << endl ;
+    cout << "5、xxxxxxxx" << endl ;
+    FLAG:
+    cout << "6、xxxxxxxx" << endl ;
+
+}
