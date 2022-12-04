46d7c41943697db54afaefb4b3121ba4080a6f58
txyuqiwang <txyuqiwang@tencent.com>
2022-12-04 08:11:29 +0800

    feat : study 二维数组的init方式 分数求和

--- arr/0503-bubbleSort.cpp
+++ arr/0503-bubbleSort.cpp
@@ -3,7 +3,7 @@
 //
 #include <iostream>
 using  namespace std ;
-int main(){
+int main0503(){
     int arr[] = { 4,2,8,0,5,7,1,3,9};
     for (int i = 0; i < 9-1 ; ++i) {
         for (int j = 0; j < 9-1-i; ++j) {
