2d3a572ac0eb794b889e7567a1e85dcce394a4d4
txyuqiwang <txyuqiwang@tencent.com>
2022-12-08 16:28:00 +0800

    feat : study 构造函数&&析构函数、

---
+++ 13对象的初始化和清理/01-ConstructorAnalyticFunction.cpp
@@ -0,0 +1,31 @@
+//
+// Created by eric on 2022/12/8.
+//
+
+#include "iostream"
+
+using namespace std;
+
+class Person1{
+public:
+    Person1(){
+        cout << " Person1 的 构造函数 调用" << endl;
+    }
+    ~Person1(){
+        cout << " Person1 的 析构函数 调用" << endl;
+    }
+
+};
+void func1301(){
+    Person1 p1;  //分配在栈区 ，直接返回 ，会调用 析构函数
+}
+int main1301(){
+    func1301();
+    Person1 p1; // //分配在栈区 ，函数不会执行完 ，只会先调用  构造函数；
+    cout<<"按任意键继续……";
+    cin.clear();
+    cin.sync();
+    cin.get();
+    return 0;
+}
+
---
+++ 13对象的初始化和清理/02ClassificationCallingConstructors.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eric on 2022/12/8.
+//
+
+#include "iostream"
+
+using namespace std;
+
+int main(){
+
+    cout<<"按任意键继续……";
+    cin.clear();
+    cin.sync();
+    cin.get();
+    return 0;
+}
